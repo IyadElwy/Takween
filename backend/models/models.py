ebbe91152a4dc0487bf929bec68b04f3a3d73bd4
Iyad ELwy <iyadelwy1@gmail.com>
2023-10-09 14:55:48 +0300

    feat: update model with date time fields

--- backend/models/models.py
+++ backend/models/models.py
@@ -9,6 +9,7 @@ class Project(Model):
     description = fields.CharField(max_length=1000, null=True)
     author = fields.CharField(max_length=20, null=True)
     dataFileName = fields.CharField(max_length=400, null=True)
+    created_at = fields.DatetimeField(auto_now_add=True)
 
     def __str__(self) -> str:
         return self.title
@@ -19,6 +20,7 @@ class Job(Model):
     title = fields.CharField(max_length=100)
     project = fields.ForeignKeyField('models.Project', related_name='Jobs')
     annotation_type = fields.CharEnumField(AnnotationType, max_length=100)
+    created_at = fields.DatetimeField(auto_now_add=True)
 
     def __str__(self) -> str:
         return f"Project: {self.project.title} | Job: {self.title}"
