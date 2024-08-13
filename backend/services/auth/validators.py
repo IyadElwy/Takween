fda8d5e053d6da09746ba412a6632964dd5171eb
Iyad ELwy <iyadelwy1@gmail.com>
2024-08-13 14:59:11 +0300

    fix: apply some style fixes

--- backend/services/auth/validators.py
+++ backend/services/auth/validators.py
@@ -2,35 +2,35 @@ from email_validator import validate_email, EmailNotValidError
 from errors import ValidationException
 
 
-def validate_user_signup_info(first_name: str, last_name: str, email: str, password: str):
+def validate_user_signup_info(first_name: str, last_name: str, email: str, password: str) -> None:
     if not first_name:
-        raise ValidationException("First name must not be empty")
+        raise ValidationException('First name must not be empty')
     if not first_name.isalpha():
-        raise ValidationException("First name must only contain alphabets")
+        raise ValidationException('First name must only contain alphabets')
 
     if not last_name:
-        raise ValidationException("Last name must not be empty")
+        raise ValidationException('Last name must not be empty')
     if not last_name.isalpha():
-        raise ValidationException("Last name must only contain alphabets")
+        raise ValidationException('Last name must only contain alphabets')
 
     if not email:
-        raise ValidationException("Email must not be empty")
+        raise ValidationException('Email must not be empty')
     try:
         validate_email(email, check_deliverability=False)
     except EmailNotValidError:
-        raise ValidationException("Email must be valid")
+        raise ValidationException('Email must be valid')
 
     if not password:
-        raise ValidationException("Password must not be empty")
+        raise ValidationException('Password must not be empty')
 
 
-def validate_user_login_info(email: str, password: str):
+def validate_user_login_info(email: str, password: str) -> None:
     if not email:
-        raise ValidationException("Email must not be empty")
+        raise ValidationException('Email must not be empty')
     try:
         validate_email(email, check_deliverability=False)
     except EmailNotValidError:
-        raise ValidationException("Email must be valid")
+        raise ValidationException('Email must be valid')
 
     if not password:
-        raise ValidationException("Password must not be empty")
+        raise ValidationException('Password must not be empty')
