1b47cfb29aebac3cc925096172fc00d76848ca50
Iyad ELwy <iyadelwy1@gmail.com>
2024-08-14 18:47:02 +0300

    fix: add cascade rules to foreign key deletes

--- backend/migrations/versions/0fd475018c21_create_jobs_table.py
+++ backend/migrations/versions/0fd475018c21_create_jobs_table.py
@@ -1,8 +1,8 @@
-"""create projects table
+"""create jobs table
 
-Revision ID: 0178c31654dc
-Revises: a9d5a68631a7
-Create Date: 2024-08-12 16:28:11.336893
+Revision ID: 0fd475018c21
+Revises: 0178c31654dc
+Create Date: 2024-08-14 16:52:39.924769
 
 """
 
@@ -11,21 +11,21 @@ from typing import Sequence, Union
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = '0178c31654dc'
-down_revision: Union[str, None] = '506022dd518d'
+revision: str = '0fd475018c21'
+down_revision: Union[str, None] = '0178c31654dc'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.execute("""CREATE TABLE Projects(
+    op.execute("""CREATE TABLE Jobs(
                 id SERIAL UNIQUE,
                 title TEXT NOT NULL,
+                project_id INTEGER NOT NULL REFERENCES Projects (id) ON DELETE CASCADE,
                 user_id_of_owner INTEGER NOT NULL REFERENCES Users (id) ON DELETE CASCADE,
-                description TEXT,
                 creation_date TIMESTAMP DEFAULT NOW() NOT NULL
                 )""")
 
 
 def downgrade() -> None:
-    op.execute('DROP TABLE Projects')
+    op.execute('DROP TABLE Jobs')
